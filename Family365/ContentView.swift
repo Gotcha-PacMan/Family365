093222e4534b5de3adb025b49c554213993552f9
WT <warat@hotmail.se>
2023-01-31 13:12:04 +0100

    empty commit

---
+++ Family365/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Family365
+//
+//  Created by Warat T on 2023-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Family365/Family365App.swift
@@ -0,0 +1,17 @@
+//
+//  Family365App.swift
+//  Family365
+//
+//  Created by Warat T on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct Family365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
