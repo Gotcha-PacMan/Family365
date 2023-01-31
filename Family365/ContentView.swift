0731a5ae3654a53e61f4454275120a8757de9678
WT <warat@hotmail.se>
2023-01-31 22:41:35 +0100

    loginView

--- Family365/ContentView.swift
+++ Family365/ContentView.swift
@@ -7,15 +7,83 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct loginView: View {
+    
+    // Propeties
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        // The View
+        VStack(spacing : 2) {
+            
+            // App Image
+            Image("icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            Text("Family365")
+                .font(.largeTitle)
+            
+            // Title login
+            Text("Login")
+                .font(.title).foregroundColor(.black)
+                .padding([.top, .bottom], 40)
+            
+            // Input textfield for login
+            TextField("Email", text: self.$email)
+                .padding()
+                .frame(width: 350, height: 60)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(.black, lineWidth: 2)
+                        .frame(width: 350, height: 60)
+                )
+            
+            SecureField("Password", text: self.$password)
+                .padding()
+                .frame(width: 350, height: 60)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(.black, lineWidth: 2)
+                        .frame(width: 350, height: 60)
+                )
+            
+                .padding(.top, 25)
+            
+            // Sign in - button
+            Button(action: {}) {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(.gray)
+                    .cornerRadius(20.0)
+            }
+            .padding(.top, 50)
+                        
+            
+        }
+    
+        
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        // Other phonemodels
+        loginView()
+            .previewInterfaceOrientation(.portrait)
+        
+        // Screen I am focusing on, Iphone 13
+        loginView()
+            .previewLayout(.fixed(width: 390, height: 844))
+        
+      
     }
 }
